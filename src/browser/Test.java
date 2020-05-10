897e8af92152b38b3186a0115928d075b8ab44be
shihabamin <amin.khandaker@gmail.com>
2020-05-10 10:53:19 -0500

    4th commit

--- src/browser/Test.java
+++ src/browser/Test.java
@@ -2,16 +2,27 @@ package browser;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Test {
 
 	public static void main(String[] args) {
 		String browser = "Firefox";
+		WebDriver driver;
 
 		if (browser.equals("Chrome")) {
 			System.setProperty("webdriver.chrome.driver",
 					"C:\\Users\\Dell\\Winter2020Selenium\\crmtechfios\\Drivers\\chromedriver.exe");
-			WebDriver driver = new ChromeDriver();
+			driver = new ChromeDriver();
+
+		}
+
+		else if (browser.equalsIgnoreCase("firefox"))
+
+		{
+			System.setProperty("webdriver.gecko.driver",
+					"C:\\Users\\Dell\\Winter2020Selenium\\crmtechfios\\Drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
 
 		}
 
