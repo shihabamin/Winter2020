16bb116ff068fbe6cc50f949dd4b2de283a13c73
shihabamin <amin.khandaker@gmail.com>
2020-05-10 10:49:40 -0500

    3rd commit

--- src/browser/Test.java
+++ src/browser/Test.java
@@ -1,10 +1,19 @@
 package browser;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String browser = "Firefox";
+
+		if (browser.equals("Chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					"C:\\Users\\Dell\\Winter2020Selenium\\crmtechfios\\Drivers\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+
+		}
 
 	}
-
 }
